4538c8315a44e0980e20a1db3713833c5f7e8c23
RJ Marsan <rjmarsan@gmail.com>
2014-01-01 14:31:50 -0800

    Adding intro

--- src/com/rj/notify/MainActivity.java
+++ src/com/rj/notify/MainActivity.java
@@ -1,18 +1,33 @@
 package com.rj.notify;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);        
+        setContentView(R.layout.activity_main);
+        setupButtons();
+    }
+    
+    private void setupButtons() {
+        ((Button)findViewById(R.id.security_settings)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				gotoSecuritySettings();
+			}
+		});
     }
 
 
@@ -25,7 +40,13 @@ public class MainActivity extends Activity {
     
     @Override
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
-    	createNotification();
+    	switch (item.getItemId()) {
+		case R.id.action_settings:
+			break;
+		case R.id.test_notification:
+			createNotification();
+			break;
+    	}
     	return super.onMenuItemSelected(featureId, item);
     }
     
@@ -34,10 +55,16 @@ public class MainActivity extends Activity {
         NotificationCompat.Builder ncomp = new NotificationCompat.Builder(this);
         ncomp.setContentTitle("My Notification");
         ncomp.setContentText("Notification Listener Service Example");
-        ncomp.setTicker("asdf");
+        ncomp.setTicker("This is a test notification.");
         ncomp.setSmallIcon(R.drawable.ic_launcher);
+        ncomp.setDefaults(Notification.DEFAULT_ALL);
         ncomp.setAutoCancel(true);
         nManager.notify((int)System.currentTimeMillis(),ncomp.build());
+    }
+    
+    private void gotoSecuritySettings() {
+    	Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+    	startActivity(intent);
     }
     
 }
